7698e60603efe42e76c40a9e85256fe8c8c8302a
honeyful <honeyful@users.noreply.github.com>
2017-09-07 11:12:14 +0900

    gui commit
    alpha

---
+++ GUI/WindowsFirewallHarden/MainFrame.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFirewallHarden
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radAllowInbound = new System.Windows.Forms.RadioButton();
+            this.radBlockInbound = new System.Windows.Forms.RadioButton();
+            this.radBlockOutbound = new System.Windows.Forms.RadioButton();
+            this.radAllowOutbound = new System.Windows.Forms.RadioButton();
+            this.lvLog = new WindowsFirewallHarden.AeroListView();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnHarden = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lvExclude = new WindowsFirewallHarden.AeroListView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnDir = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(308, 205);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(93, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "SaveLog";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radBlockInbound);
+            this.groupBox1.Controls.Add(this.radAllowInbound);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 70);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Inbound";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radBlockOutbound);
+            this.groupBox2.Controls.Add(this.radAllowOutbound);
+            this.groupBox2.Location = new System.Drawing.Point(221, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 70);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Outbound";
+            // 
+            // radAllowInbound
+            // 
+            this.radAllowInbound.AutoSize = true;
+            this.radAllowInbound.Location = new System.Drawing.Point(6, 20);
+            this.radAllowInbound.Name = "radAllowInbound";
+            this.radAllowInbound.Size = new System.Drawing.Size(54, 16);
+            this.radAllowInbound.TabIndex = 0;
+            this.radAllowInbound.Text = "Allow";
+            this.radAllowInbound.UseVisualStyleBackColor = true;
+            // 
+            // radBlockInbound
+            // 
+            this.radBlockInbound.AutoSize = true;
+            this.radBlockInbound.Checked = true;
+            this.radBlockInbound.Location = new System.Drawing.Point(6, 42);
+            this.radBlockInbound.Name = "radBlockInbound";
+            this.radBlockInbound.Size = new System.Drawing.Size(54, 16);
+            this.radBlockInbound.TabIndex = 1;
+            this.radBlockInbound.TabStop = true;
+            this.radBlockInbound.Text = "Block";
+            this.radBlockInbound.UseVisualStyleBackColor = true;
+            // 
+            // radBlockOutbound
+            // 
+            this.radBlockOutbound.AutoSize = true;
+            this.radBlockOutbound.Checked = true;
+            this.radBlockOutbound.Location = new System.Drawing.Point(6, 42);
+            this.radBlockOutbound.Name = "radBlockOutbound";
+            this.radBlockOutbound.Size = new System.Drawing.Size(54, 16);
+            this.radBlockOutbound.TabIndex = 3;
+            this.radBlockOutbound.TabStop = true;
+            this.radBlockOutbound.Text = "Block";
+            this.radBlockOutbound.UseVisualStyleBackColor = true;
+            // 
+            // radAllowOutbound
+            // 
+            this.radAllowOutbound.AutoSize = true;
+            this.radAllowOutbound.Location = new System.Drawing.Point(6, 20);
+            this.radAllowOutbound.Name = "radAllowOutbound";
+            this.radAllowOutbound.Size = new System.Drawing.Size(54, 16);
+            this.radAllowOutbound.TabIndex = 2;
+            this.radAllowOutbound.Text = "Allow";
+            this.radAllowOutbound.UseVisualStyleBackColor = true;
+            // 
+            // lvLog
+            // 
+            this.lvLog.FullRowSelect = true;
+            this.lvLog.Location = new System.Drawing.Point(6, 20);
+            this.lvLog.Name = "lvLog";
+            this.lvLog.Size = new System.Drawing.Size(395, 179);
+            this.lvLog.TabIndex = 3;
+            this.lvLog.UseCompatibleStateImageBehavior = false;
+            this.lvLog.View = System.Windows.Forms.View.Details;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(172, 205);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(130, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnHarden
+            // 
+            this.btnHarden.Location = new System.Drawing.Point(6, 205);
+            this.btnHarden.Name = "btnHarden";
+            this.btnHarden.Size = new System.Drawing.Size(160, 23);
+            this.btnHarden.TabIndex = 5;
+            this.btnHarden.Text = "Harden";
+            this.btnHarden.UseVisualStyleBackColor = true;
+            this.btnHarden.Click += new System.EventHandler(this.btnHarden_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnRemove);
+            this.groupBox3.Controls.Add(this.btnFile);
+            this.groupBox3.Controls.Add(this.btnDir);
+            this.groupBox3.Controls.Add(this.lvExclude);
+            this.groupBox3.Location = new System.Drawing.Point(427, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(409, 317);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Exclude";
+            // 
+            // lvExclude
+            // 
+            this.lvExclude.FullRowSelect = true;
+            this.lvExclude.Location = new System.Drawing.Point(6, 15);
+            this.lvExclude.Name = "lvExclude";
+            this.lvExclude.Size = new System.Drawing.Size(397, 260);
+            this.lvExclude.TabIndex = 4;
+            this.lvExclude.UseCompatibleStateImageBehavior = false;
+            this.lvExclude.View = System.Windows.Forms.View.Details;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lvLog);
+            this.groupBox4.Controls.Add(this.btnSave);
+            this.groupBox4.Controls.Add(this.btnHarden);
+            this.groupBox4.Controls.Add(this.btnReset);
+            this.groupBox4.Location = new System.Drawing.Point(12, 88);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(409, 241);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // btnDir
+            // 
+            this.btnDir.Location = new System.Drawing.Point(6, 281);
+            this.btnDir.Name = "btnDir";
+            this.btnDir.Size = new System.Drawing.Size(160, 23);
+            this.btnDir.TabIndex = 6;
+            this.btnDir.Text = "Add Directory";
+            this.btnDir.UseVisualStyleBackColor = true;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(172, 281);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(136, 23);
+            this.btnFile.TabIndex = 7;
+            this.btnFile.Text = "Add File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(314, 281);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(89, 23);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 340);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainFrame";
+            this.Load += new System.EventHandler(this.MainFrame_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radBlockInbound;
+        private System.Windows.Forms.RadioButton radAllowInbound;
+        private System.Windows.Forms.RadioButton radBlockOutbound;
+        private System.Windows.Forms.RadioButton radAllowOutbound;
+        private AeroListView lvLog;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnHarden;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnDir;
+        private AeroListView lvExclude;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ GUI/WindowsFirewallHarden/MainFrame.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using NetFwTypeLib;
+
+namespace WindowsFirewallHarden
+{
+    public partial class MainFrame : Form
+    {
+        private const string ext = "*.exe";
+        private readonly string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        private readonly string programFilesx86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+        private readonly string winDir = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+        private readonly string appRoaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private readonly string appLocal = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrame_Load(object sender, EventArgs e)
+        {
+            lvLog.Columns.Add("Path", 300);
+            lvLog.Columns.Add("Status", 100);
+            lvExclude.Columns.Add("Path", 300);
+            lvExclude.Columns.Add("Type", 100);
+        }
+
+        #region Firewall
+        private void fwHarden()
+        {
+            //var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+            var currentProfiles = fwPolicy2.CurrentProfileTypes;
+            List<string> ruleList = new List<string>();
+
+            foreach (INetFwRule rule in fwPolicy2.Rules)
+            {
+                ruleList.Add(rule.Name.ToLower());
+            }
+
+            string[] rows = { programFiles, programFilesx86, winDir, appLocal, appRoaming };
+
+            foreach (var dir in rows)
+            {
+                foreach (var file in GetFiles(dir, ext))
+                {
+                    var match = ruleList
+                    .FirstOrDefault(stringToCheck => stringToCheck.Contains(file.ToLower()));
+
+                    if (match != null)
+                    {
+                        string[] items = { file, "Already" };
+                        ListViewItem lvi = new ListViewItem(items);
+                        addItemToListView(lvLog, lvi);
+
+                        continue;
+                    }
+                    if (checkSignature(file))
+                    {
+                        string[] items = { file, "Allow" };
+                        ListViewItem lvi = new ListViewItem(items);
+                        addItemToListView(lvLog, lvi);
+
+                        allowRule(file);
+                    }
+                    else
+                    {
+                        string[] items = { file, "Block" };
+                        ListViewItem lvi = new ListViewItem(items);
+                        addItemToListView(lvLog, lvi);
+
+                        blockRule(file);
+                    }
+                }
+            }
+            
+                initRule();
+
+            //sw.Stop();
+            //var elapsedMs = sw.ElapsedMilliseconds;
+            //Console.WriteLine($"Time:{elapsedMs}");
+        }
+
+        private void initRule()
+        {
+            Type netFwPolicy2Type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 mgr = (INetFwPolicy2)Activator.CreateInstance(netFwPolicy2Type);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, true);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, true);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+
+            mgr.set_BlockAllInboundTraffic(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, true);
+            mgr.set_DefaultInboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+            mgr.set_DefaultOutboundAction(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC, NET_FW_ACTION_.NET_FW_ACTION_BLOCK);
+        }
+
+        private void allowRule(string fileName)
+        {
+            INetFwRule fwRule = (INetFwRule)Activator.CreateInstance(
+                 Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            fwRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            fwRule.Enabled = true;
+            fwRule.InterfaceTypes = "All";
+            fwRule.Name = fileName;
+            fwRule.ApplicationName = fileName;
+            INetFwPolicy2 fwPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            fwRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            fwPolicy.Rules.Add(fwRule);
+        }
+
+        private void blockRule(string fileName)
+        {
+            INetFwRule fwRule = (INetFwRule)Activator.CreateInstance(
+                Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            fwRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+            fwRule.Enabled = true;
+            fwRule.InterfaceTypes = "All";
+            fwRule.Name = fileName;
+            fwRule.ApplicationName = fileName;
+            INetFwPolicy2 fwPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            fwRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT;
+            fwPolicy.Rules.Add(fwRule);
+        }
+
+        private void fwReset()
+        {
+            Type netFwPolicy2Type = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 mgr = (INetFwPolicy2)Activator.CreateInstance(netFwPolicy2Type);
+            mgr.RestoreLocalFirewallDefaults();
+        }
+
+        private bool checkSignature(string fileName)
+        {
+            var certChain = new X509Chain();
+            var cert = default(X509Certificate2);
+            bool isChainValid = false;
+            try
+            {
+                var signer = X509Certificate.CreateFromSignedFile(fileName);
+                cert = new X509Certificate2(signer);
+            }
+            catch
+            {
+                return isChainValid;
+            }
+
+            certChain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+            certChain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+            certChain.ChainPolicy.UrlRetrieval‎Timeout = new TimeSpan(0, 1, 0);
+            certChain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+            isChainValid = certChain.Build(cert);
+
+            return isChainValid;
+
+        }
+
+        private IEnumerable<string> GetFiles(string path, string exts)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue(path);
+            while (q.Count > 0)
+            {
+                path = q.Dequeue();
+                try
+                {
+                    foreach (string subDir in Directory.GetDirectories(path))
+                    {
+                        q.Enqueue(subDir);
+                    }
+                }
+                catch
+                {
+
+                }
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(path, exts);
+                }
+                catch
+                {
+
+                }
+                if (files != null)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        yield return files[i];
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region ListView
+        private delegate void addToListViewDelegate(ListView lv, ListViewItem lvi);
+        private void addItemToListView(ListView lv, ListViewItem lvi)
+        {
+            if (lv.InvokeRequired)
+            {
+                Invoke(new addToListViewDelegate(addItemToListView), new object[] { lv, lvi });
+                return;
+            }
+            lv.Items.Add(lvi);
+        }
+        #endregion
+
+        private void btnHarden_Click(object sender, EventArgs e)
+        {
+            new Thread(fwHarden).Start();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            fwReset();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
